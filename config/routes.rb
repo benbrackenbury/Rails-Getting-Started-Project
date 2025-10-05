0146d4105a20da647795be0c6a9421a13f3c29dc
Ben Brackenbury <benbrack22@icloud.com>
2025-10-05 17:03:31 +0100

    16.2: inventory subscription

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resource :session
-  resources :passwords, param: :token
+  root "products#index"
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resource :session
+  resources :passwords, param: :token
   resources :products
-  root "products#index"
+  resources :subscribers, only: [ :create ]
 end
